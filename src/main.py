ac390c9d6c01025ff3555a243481704239af227d
vash96 <brian.riccardi96@gmail.com>
2023-03-01 18:15:20 +0100

    Scrambler/Descrambler as external plugin

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import scraml
+import remote_ml
 import numpy as np
 from sklearn import datasets
 from sklearn import decomposition
@@ -8,15 +9,26 @@ def main():
     data_matrix = iris.data
     print("Using Iris data matrix:\n\n{}\n\n"\
           .format(data_matrix[:10,]))
-    scrambled, perm = scraml.scramble(data_matrix)
+    
 
+
+    # Interaction with preservation plugin
+    preservation_plugin = scraml.PreservationPlugin()
+
+    scrambled, perm = preservation_plugin.scramble(data_matrix)
     print("Scramble matrix is:\n\n{}\n\n"\
           .format(scrambled[:10, ]))
-
-    descrambled = scraml.descramble(scrambled, perm)
+    descrambled = preservation_plugin.descramble(scrambled, perm)
     print("Is de-scrambled == original? {}\n\n"\
           .format(np.array_equal(data_matrix, descrambled)))
-    pass
+    
+
+
+    # Interaction with the remote_ml sever
+    server = remote_ml.RemoteML()
+    original_pc = server.pca(data_matrix)
+    scrambled_pc = server.pca(scrambled)
+    descrambled_pc = server.pca(descrambled)
 
 if __name__ == "__main__":
     main()
--- src/scraml.py
+++ src/scraml.py
@@ -1,21 +1,22 @@
 import numpy as np
 
-def scramble(data_matrix, cols_scramble = False):
-    perm_size = data_matrix.shape[cols_scramble]
-    perm = np.random.permutation(perm_size)
-    
-    if cols_scramble:
-        scram_data_matrix = data_matrix[:, perm]
-    else:
-        scram_data_matrix = data_matrix[perm]
-    
-    return (scram_data_matrix, perm)
+class PreservationPlugin:
+    def scramble(self, data_matrix, cols_scramble = False):
+        perm_size = data_matrix.shape[cols_scramble]
+        perm = np.random.permutation(perm_size)
+        
+        if cols_scramble:
+            scram_data_matrix = data_matrix[:, perm]
+        else:
+            scram_data_matrix = data_matrix[perm]
+        
+        return (scram_data_matrix, perm)
 
 
-def descramble(scram_data_matrix, perm, cols_scramble = False):
-    perm = np.argsort(perm) # Get inverse permutation
+    def descramble(self, scram_data_matrix, perm, cols_scramble = False):
+        perm = np.argsort(perm) # Get inverse permutation
 
-    if cols_scramble:
-        return scram_data_matrix[:, perm]
-    else:
-        return scram_data_matrix[perm]
+        if cols_scramble:
+            return scram_data_matrix[:, perm]
+        else:
+            return scram_data_matrix[perm]
