6487aea5b4dc99bbf15c30a4ae144ce622f67767
vash96 <brian.riccardi96@gmail.com>
2023-03-01 14:56:55 +0100

    testing scrambling/descrambling

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 import scraml
-
-
-
+import numpy as np
+from sklearn import datasets
+from sklearn import decomposition
 
 def main():
+    iris = datasets.load_iris()
+    data_matrix = iris.data
+    print("Using Iris data matrix:\n\n{}\n\n"\
+          .format(data_matrix[:10,]))
+    scrambled, perm = scraml.scramble(data_matrix)
+
+    print("Scramble matrix is:\n\n{}\n\n"\
+          .format(scrambled[:10, ]))
+
+    descrambled = scraml.descramble(scrambled, perm)
+    print("Is de-scrambled == original? {}\n\n"\
+          .format(np.array_equal(data_matrix, descrambled)))
     pass
 
 if __name__ == "__main__":
