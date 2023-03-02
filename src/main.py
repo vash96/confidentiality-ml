Fixed imports, added scaling to RemoteML.pca()

src/main.py
@@ -4,11 +4,14 @@ import numpy as np
 from sklearn import datasets
 from sklearn import decomposition
 
+def head(mat, n=10):
+    return mat[:n,]
+
 def main():
     iris = datasets.load_iris()
     data_matrix = iris.data
-    print("Using Iris data matrix:\n\n{}\n\n"\
-          .format(data_matrix[:10,]))
+    print("Using Iris data matrix:\n{}\n    .........\nWith {} observations over {} features.\n"\
+          .format(head(data_matrix), data_matrix.shape[0], data_matrix.shape[1]))
     
 
 
@@ -16,8 +19,8 @@ def main():
     preservation_plugin = scraml.PreservationPlugin()
 
     scrambled, perm = preservation_plugin.scramble(data_matrix)
-    print("Scramble matrix is:\n\n{}\n\n"\
-          .format(scrambled[:10, ]))
+    print("Scrambled matrix:\n{}\n    .........\n"\
+          .format(head(scrambled)))
     descrambled = preservation_plugin.descramble(scrambled, perm)
     print("Is de-scrambled == original? {}\n\n"\
           .format(np.array_equal(data_matrix, descrambled)))
@@ -29,6 +32,8 @@ def main():
     original_pc = server.pca(data_matrix)
     scrambled_pc = server.pca(scrambled)
     descrambled_pc = server.pca(descrambled)
+
+    print("{}\n{}\n{}".format(original_pc, scrambled_pc, descrambled_pc))
 
 if __name__ == "__main__":
     main()

src/scraml.py
@@ -1,9 +1,10 @@
-import numpy as np
+from numpy.random import permutation
+from numpy import argsort
 
 class PreservationPlugin:
     def scramble(self, data_matrix, cols_scramble = False):
         perm_size = data_matrix.shape[cols_scramble]
-        perm = np.random.permutation(perm_size)
+        perm = permutation(perm_size)
         
         if cols_scramble:
             scram_data_matrix = data_matrix[:, perm]
@@ -14,7 +15,7 @@ class PreservationPlugin:
 
 
     def descramble(self, scram_data_matrix, perm, cols_scramble = False):
-        perm = np.argsort(perm) # Get inverse permutation
+        perm = argsort(perm) # Get inverse permutation
 
         if cols_scramble:
             return scram_data_matrix[:, perm]