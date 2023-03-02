39a12da88264a80cfd2dd002837d544bc2510e53
vash96 <brian.riccardi96@gmail.com>
2023-03-02 18:10:55 +0100

    Added svd

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-import scraml
-import remote_ml
-import numpy as np
+from numpy import array_equal
 from sklearn import datasets
 from sklearn import decomposition
+
+import scraml
+import remote_ml
 
 def head(mat, n=10):
     return mat[:n,]
@@ -23,17 +24,27 @@ def main():
           .format(head(scrambled)))
     descrambled = preservation_plugin.descramble(scrambled, perm)
     print("Is de-scrambled == original? {}\n\n"\
-          .format(np.array_equal(data_matrix, descrambled)))
+          .format(array_equal(data_matrix, descrambled)))
     
 
 
     # Interaction with the remote_ml sever
     server = remote_ml.RemoteML()
+
+    # Playing with PCA
     original_pc = server.pca(data_matrix)
     scrambled_pc = server.pca(scrambled)
     descrambled_pc = server.pca(descrambled)
 
-    print("{}\n{}\n{}".format(original_pc, scrambled_pc, descrambled_pc))
+    print("{}\n{}\n{}\n\n".format(original_pc, scrambled_pc, descrambled_pc))
+
+    # Playing with SVD
+    original_s = server.svd(data_matrix)
+    scrambled_s = server.svd(scrambled)
+    descrambled_s = server.svd(descrambled)
+
+    print("{}\n{}\n{}\n\n".format(original_s, scrambled_s, descrambled_s))
+
 
 if __name__ == "__main__":
     main()
--- src/remote_ml.py
+++ src/remote_ml.py
@@ -1,4 +1,4 @@
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.preprocessing import StandardScaler
 
 
@@ -8,4 +8,11 @@ class RemoteML:
         data_matrix = StandardScaler().fit_transform(data_matrix)
 
         # Return principal components of data_matrix
-        return PCA().fit(data_matrix)
+        return PCA().fit(data_matrix).components_
+        
+    def svd(self, data_matrix):
+        # Scaling and centering
+        data_matrix = StandardScaler().fit_transform(data_matrix)
+
+        # Return vector of singular values
+        return TruncatedSVD().fit(data_matrix).singular_values_
