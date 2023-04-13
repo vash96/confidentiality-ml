79eb16f7f174b88ff52c462957d3ec7cf96be24a
vash96 <brian.riccardi96@gmail.com>
2023-04-13 14:51:46 +0200

    cleaning

--- src/python/remote_ml_pb2.pyi
+++
@@ -1,8 +0,0 @@
-import basic_types_pb2 as _basic_types_pb2
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-from basic_types_pb2 import Empty
-from basic_types_pb2 import Float
-from basic_types_pb2 import NDArray
-DESCRIPTOR: _descriptor.FileDescriptor
